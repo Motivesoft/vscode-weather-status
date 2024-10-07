c4633ae63206242de72b0f76308f19b47f77c04d
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-10-07 19:04:20 +0000

    Add configuration

--- src/extension.ts
+++ src/extension.ts
@@ -16,16 +16,25 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const commandId = 'vscode-weather-status.update';
 	context.subscriptions.push(vscode.commands.registerCommand(commandId, () => {
-		vscode.window.showInformationMessage('Updating weather status');
 		updateWeatherStatus();
 	}));
 
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(event => {
+		if(event.affectsConfiguration('vscode-weather-status.location')
+			|| event.affectsConfiguration('vscode-weather-status.format')
+			|| event.affectsConfiguration('vscode-weather-status.language')) {
+			// Update the display - only do this if a pertinent setting has changed
+			console.log('Configuration changed!');
+			updateWeatherStatus();
+		}
+	}));
+	
 	// TODO: Make the alignment and priority configurable? 
 	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
 	statusBarItem.command = commandId;
 	context.subscriptions.push(statusBarItem);
 
-	// TODO check this is being executed
+	// Set the weather status on activation
 	vscode.commands.executeCommand(commandId);
 
 	// Cause an update on a regular basis - every 60 minutes
@@ -44,16 +53,15 @@ export function deactivate()
 }
 
 async function updateWeatherStatus() {
-	//console.log('Updating weather status');
-
 	await getWeatherData();
 }
 
 async function getWeatherData(): Promise<void> {
 	const config = vscode.workspace.getConfiguration("vscode-weather-status");
-	const location = config.get("location","alicante");
+	const location = config.get("location","");
 	const formatString = config.get("format","%C %t %h %w");
-	const langCode = config.get("lang","");
+	const langCode = config.get("language","");
+	const showMessage = config.get("update-message","false");
 
 	try {
 		const response = await axios.get('https://wttr.in/'+location, {
@@ -62,10 +70,19 @@ async function getWeatherData(): Promise<void> {
 				lang: langCode
 			}
 		});
-
+		
+		if(showMessage) {
+			vscode.window.showInformationMessage('Updating weather status');
+		}
+	
+		console.log('Obtained updated weather status');
 		statusBarItem.text = response.data;
 	} catch (error) {
-		console.log('Failed to get weather update: {error}');
+		if(showMessage) {
+			vscode.window.showWarningMessage('Failed to update weather status');
+		}
+
+		console.log('Failed to get weather update: '+error);
 		statusBarItem.text = "n/a";
 	}
 
